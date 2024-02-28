71fb528ea28ac84b0b0fadadf7a51d2298e6db73
Sarah-dev-web <emailaddress@yahoo.com>
2024-02-28 23:10:10 +0100

    Réorganisation page d'accueil

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -81,7 +81,7 @@ function shoWorks(arrayOfWorks){
 function showCategories(arrayOfCategories){
     let categoriesHtml = ""
     categoriesHtml = `
-        <button class="boutons-filtres btn-actif">Tous</button>
+        <button class="boutons-filtres">Tous</button>
         `
     arrayOfCategories.forEach(cat => {
         categoriesHtml += `
